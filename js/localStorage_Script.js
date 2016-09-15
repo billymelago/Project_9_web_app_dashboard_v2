6d7e31fbee2c4632e56ffd8f6071a5b74cfe6e7e
Nicholas Serra <nicks@ydekproductions.com>
2016-09-14 19:43:42 -0600

    Fix local storage stuff

--- js/localStorage_Script.js
+++ js/localStorage_Script.js
@@ -1,51 +1,16 @@
-
-
-if(!localStorage.getItem('email_pref')) {
-    loadStoragePref();
-} else {
-    setStoragePref();
+if(localStorage.getItem('email_pref')) {
+  document.getElementById('cmn-toggle-7').checked = JSON.parse(localStorage.getItem('email_pref'));
+  document.getElementById('cmn-toggle-8').checked = JSON.parse(localStorage.getItem('profile_pref')); 
+  document.getElementById('cmn-select-9').value = localStorage.getItem('timezone_pref');
 }
 
-function setStoragePref() {
-    var sendEmailNotifications = localStorage.getItem('email_pref');
-    var setProfileToPublic = localStorage.getItem('profile_pref');
-    var timezoneChoice = localStorage.getItem('timezone_pref');
-    
-    document.getElementById('cmn-toggle-7').checked = sendEmailNotifications; //true or false
-    document.getElementById('cmn-toggle-8').checked = setProfileToPublic; 
-    document.getElementById('cmn-select-9').value = timezoneChoice;
-    console.log(sendEmailNotifications);
-    
-    
-}
-
-function loadStoragePref() {
-    var result = function(r) {
-        if(document.getElementById('cmn-toggle-7').checked){
-            r = 'true'
-    } else if(!document.getElementById('cmn-toggle-7').checked){
-            r = 'false';
-        }
-    };
-                                    // true or false    
-    localStorage.setItem('email_pref', 'result(r)');
-    localStorage.setItem('profile_pref', document.getElementById('cmn-toggle-8').checked);
-    localStorage.setItem('timezone_pref', document.getElementById('cmn-select-9').value);
-    
-    setStoragePref();
-}
-
-
-
-//when 'save' is clicked
-//go and see if cmn-toggle is checked. true or false
-//store result in setItem(' _pref', result) true or false
-
-
-
-document.getElementById("save").addEventListener('click', loadStoragePref);
+document.getElementById("save").addEventListener('click', function() {
+  localStorage.setItem('email_pref', document.getElementById('cmn-toggle-7').checked);
+  localStorage.setItem('profile_pref', document.getElementById('cmn-toggle-8').checked);
+  localStorage.setItem('timezone_pref', document.getElementById('cmn-select-9').value);
+});
 
 document.getElementById("cancel").addEventListener("click", function() {
   localStorage.clear();
-  location.reload();  
-}); 
+  location.reload();
+});
